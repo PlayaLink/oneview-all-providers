65352358725fe4c76c7a8db9305c117b5943d5d6
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2025-07-16 13:46:22 -0400

    Fixes z-index on SidePanel and removes unused json objects from pervious data seeding

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -612,7 +612,7 @@ if (template?.DetailsComponent) {
 
   return (
     <div
-      className={`fixed top-0 right-0 h-full bg-white transform transition-transform duration-300 ease-in-out z-50 flex flex-col ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      className={`fixed top-0 right-0 h-full bg-white transform transition-transform duration-300 ease-in-out z-[10000] flex flex-col ${isOpen ? "translate-x-0" : "translate-x-full"}`}
       role="dialog"
       aria-modal="true"
       aria-label={headerText}
