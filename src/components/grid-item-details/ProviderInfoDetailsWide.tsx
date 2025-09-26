7772484806c5e5c389954e822d49d324202e4fcf
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2025-09-26 14:06:50 -0400

    feat: add responsive label positioning to ProviderInfoDetailsWide
    - Import useIsMobile hook for screen size detection
    - Convert component to function component to use hooks
    - Set labelPosition to 'left' on mobile (xs) screens and 'above' on larger screens
    - Apply responsive label positioning to all input fields across all sections
    - Improves mobile usability while maintaining desktop layout

--- src/components/grid-item-details/ProviderInfoDetailsWide.tsx
+++ src/components/grid-item-details/ProviderInfoDetailsWide.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CollapsibleSection from '../CollapsibleSection';
 import { renderFieldComponent } from './getInputType';
 import { generateProviderName, generateDefaultHeaderText } from '@/lib/utils';
+import { useIsMobile } from '@/hooks/use-mobile';
 import {
   PREFIX_OPTIONS,
   SUFFIX_OPTIONS,
@@ -326,64 +327,69 @@ const contactInfoFields = providerInfoWideFieldGroups[2].fields;
 const emergencyContactFields = providerInfoWideFieldGroups[3].fields;
 const identificationFields = providerInfoWideFieldGroups[4].fields;
 
-const ProviderInfoDetailsWide = ({ formValues, handleChange }) => (
-  <div className="flex flex-col gap-6" data-testid="provider-info-details-wide" role="main">
-    {/* Provider Name Section */}
-    <CollapsibleSection title="Provider Name">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-        {providerNameFields.map((field) => (
-          <React.Fragment key={field.key}>
-            {renderFieldComponent({ field, formValues, handleChange, labelPosition: 'above' })}
-          </React.Fragment>
-        ))}
-      </div>
-    </CollapsibleSection>
+const ProviderInfoDetailsWide = ({ formValues, handleChange }) => {
+  const isMobile = useIsMobile();
+  const labelPosition = isMobile ? 'left' : 'above';
 
-    {/* Title, Specialty & Classifications Section */}
-    <CollapsibleSection title="Title, Specialty & Classifications">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-        {titleSpecialtyFields.map((field) => (
-          <React.Fragment key={field.key}>
-            {renderFieldComponent({ field, formValues, handleChange, labelPosition: 'above' })}
-          </React.Fragment>
-        ))}
-      </div>
-    </CollapsibleSection>
+  return (
+    <div className="flex flex-col gap-6" data-testid="provider-info-details-wide" role="main">
+      {/* Provider Name Section */}
+      <CollapsibleSection title="Provider Name">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
+          {providerNameFields.map((field) => (
+            <React.Fragment key={field.key}>
+              {renderFieldComponent({ field, formValues, handleChange, labelPosition })}
+            </React.Fragment>
+          ))}
+        </div>
+      </CollapsibleSection>
 
-    {/* Contact Info Section */}
-    <CollapsibleSection title="Contact Info">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-        {contactInfoFields.map((field) => (
-          <React.Fragment key={field.key}>
-            {renderFieldComponent({ field, formValues, handleChange, labelPosition: 'above' })}
-          </React.Fragment>
-        ))}
-      </div>
-    </CollapsibleSection>
+      {/* Title, Specialty & Classifications Section */}
+      <CollapsibleSection title="Title, Specialty & Classifications">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
+          {titleSpecialtyFields.map((field) => (
+            <React.Fragment key={field.key}>
+              {renderFieldComponent({ field, formValues, handleChange, labelPosition })}
+            </React.Fragment>
+          ))}
+        </div>
+      </CollapsibleSection>
 
-    {/* Emergency Contact Section */}
-    <CollapsibleSection title="Emergency Contact">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-        {emergencyContactFields.map((field) => (
-          <React.Fragment key={field.key}>
-            {renderFieldComponent({ field, formValues, handleChange, labelPosition: 'above' })}
-          </React.Fragment>
-        ))}
-      </div>
-    </CollapsibleSection>
+      {/* Contact Info Section */}
+      <CollapsibleSection title="Contact Info">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
+          {contactInfoFields.map((field) => (
+            <React.Fragment key={field.key}>
+              {renderFieldComponent({ field, formValues, handleChange, labelPosition })}
+            </React.Fragment>
+          ))}
+        </div>
+      </CollapsibleSection>
 
-    {/* Identification Section */}
-    <CollapsibleSection title="Identification">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-        {identificationFields.map((field) => (
-          <React.Fragment key={field.key}>
-            {renderFieldComponent({ field, formValues, handleChange, labelPosition: 'above' })}
-          </React.Fragment>
-        ))}
-      </div>
-    </CollapsibleSection>
-  </div>
-);
+      {/* Emergency Contact Section */}
+      <CollapsibleSection title="Emergency Contact">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
+          {emergencyContactFields.map((field) => (
+            <React.Fragment key={field.key}>
+              {renderFieldComponent({ field, formValues, handleChange, labelPosition })}
+            </React.Fragment>
+          ))}
+        </div>
+      </CollapsibleSection>
+
+      {/* Identification Section */}
+      <CollapsibleSection title="Identification">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
+          {identificationFields.map((field) => (
+            <React.Fragment key={field.key}>
+              {renderFieldComponent({ field, formValues, handleChange, labelPosition })}
+            </React.Fragment>
+          ))}
+        </div>
+      </CollapsibleSection>
+    </div>
+  );
+};
 
 export const providerInfoWideTemplate = {
   id: 'provider_info_wide',
